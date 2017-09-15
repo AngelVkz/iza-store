60d39e0f79a03c3774beea156a820980c1fff1e1
AngelVkz <angel.vkz@outlook.com>
2017-09-15 04:16:00 -0500

    Big-img on hover

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Table.css'
+
+const Img = ({url}) => (
+    <div id="thumbwrap">
+        <div className="thumb" >
+            <img src={url} alt=""/>
+            <span><img src={url} alt=""/></span>
+        </div>
+    </div>
+)
 
 const Table = ({items, filterText}) => {
 
-    const filteredItems = items.filter( item => item.nombre.indexOf(filterText) !== -1);
+    const filteredItems = items.filter( item =>{ 
+        return item.nombre.toLowerCase().indexOf(filterText.toLowerCase()) !== -1    
+    });
     
 
     return (
@@ -11,7 +23,7 @@ const Table = ({items, filterText}) => {
             <table className="table table-hover table-bordered">
                 <thead>
                     <tr>
-                        <th className="text-center">img</th>
+                        <th className="text-center">Imagen</th>
                         <th className="text-center">Nombre</th>
                         <th className="text-center">$</th>
                     </tr>
@@ -21,9 +33,12 @@ const Table = ({items, filterText}) => {
                    {
                     filteredItems.map( item => (
                         <tr>
-                        <img src={item.img} width="60px"/>
-                        <td className=""><strong>{item.nombre}</strong></td>
-                        <td className="text-danger"><strong>{item.precio}</strong></td>
+                        <td className="img-w">
+                            <img src={item.img} width="60px" />
+                            <img src={item.img} width="60px" className="img-i"/>
+                        </td>
+                        <td className=""><h3>{item.nombre}</h3></td>
+                        <td className="text-danger"><h3>{item.precio}</h3></td>
                         </tr>     
                     ))
                    }               
